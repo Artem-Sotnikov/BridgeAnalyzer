c87235034f2087ff227231d0a66a6001482ef8a7
Code-Titan <artemis.sotnikov12@gmail.com>
2020-07-12 11:17:57 -0400

    Debug

---
+++ main_frame.py
@@ -0,0 +1,24 @@
+'''
+Created on Jul 12, 2020
+
+@author: Artem Sotnikov
+'''
+
+from PyQt5.QtWidgets import QFrame, QVBoxLayout
+
+
+
+class MainFrame(QFrame):    
+    def __init__(self):
+        super().__init__()
+        
+        #self.menu_bar = MainFrameMenubar()
+        #self.graphics_widget = GraphicsView()
+        
+        print('main window created')
+        self.setLayout(QVBoxLayout())
+        self.layout().addWidget(self.menu_bar)
+        self.layout().addWidget(self.graphics_widget)
+        
+        
+        self.show()
