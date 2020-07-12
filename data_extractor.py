c9eaaf0cce5f720d164253939791e85fffbe1511
Maximo van der Raadt <37664376+InlandTaipan@users.noreply.github.com>
2020-07-12 13:08:10 -0600

    Working 1.0
    still need to add post processing and cost analysis

--- data_extractor.py
+++ data_extractor.py
@@ -19,8 +19,8 @@ class DataExtractor():
         output_list = []
         
         for row in reader[1:]:
-            end_vertex = ans.Vertex(row[2], row[3])
-            start_vertex = ans.Vertex(row[4], row[5])
+            end_vertex = (float(row[2]), float(row[3]))
+            start_vertex = (float(row[4]), float(row[5]))
             
             ins_list = [start_vertex, end_vertex]
             output_list.append(ins_list)
@@ -28,5 +28,5 @@ class DataExtractor():
         print(output_list)
         return output_list
 # Test
-extractor = DataExtractor()
-extractor.extract_data(r"C:/Users/Artem Sotnikov/Documents/test_data_extract3.csv")
+#extractor = DataExtractor()
+#extractor.extract_data(r"C:/Users/Artem Sotnikov/Documents/test_data_extract3.csv")
--- main.py
+++ main.py
@@ -4,19 +4,22 @@ Created on Jul 12, 2020
 @author: Artem Sotnikov
 '''
 
-
-from main_frame import MainFrame
-
-import anastruct as anast
-
-from PyQt5.QtWidgets import QApplication
+import coolCalculator
+import anastruct
+from data_extractor import DataExtractor
 
 if __name__ == "__main__":
     print('Bridge Analyser active!')
     
-    app = QApplication([])
-    main_frame = MainFrame()
+    extractor = DataExtractor() 
+    output_list = extractor.extract_data("test_data_extract3.csv")    
 
-    app.exec()
+    ss = anastruct.SystemElements()
     
+    nodeDict = coolCalculator.createMembers(output_list, ss)
+
+    print(nodeDict)
     
+    coolCalculator.addSupports(ss, nodeDict)
+    coolCalculator.addLoads(ss, nodeDict)
+    coolCalculator.runSimulation(ss)
