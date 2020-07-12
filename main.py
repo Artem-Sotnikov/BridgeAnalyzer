b24597f2dc8616d291b8b206adc5b84892130044
Code-Titan <artemis.sotnikov12@gmail.com>
2020-07-12 11:01:44 -0400

    Make initial graphics setup

---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+Created on Jul 12, 2020
+
+@author: Artem Sotnikov
+'''
+
+
+from main_frame import MainFrame
+
+import anastruct as anast
+
+from PyQt5.QtWidgets import QApplication
+
+if __name__ == "__main__":
+    print('Bridge Analyser active!')
+    
+    app = QApplication([])
+    main_frame = MainFrame()
+    app.exec()
