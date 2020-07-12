82ea84bfa58facc38cde9ca80aa9fbd7d5afbf1b
Code-Titan <artemis.sotnikov12@gmail.com>
2020-07-12 11:01:44 -0400

    Make initial graphics setup

---
+++ graphics_view.py
@@ -0,0 +1,31 @@
+'''
+Created on Jul 12, 2020
+
+@author: Artem Sotnikov
+'''
+from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene
+from PyQt5.QtCore import QRectF
+from PyQt5.Qt import Qt
+
+
+class GraphicsView(QGraphicsView):
+    PAN_BOUNDARY = QRectF(-1500, -1000, 3000, 2000)
+    
+    
+    def __init__(self):
+        self.g_scene = QGraphicsScene()
+        super().__init__(self.g_scene)
+        self.setDragMode(QGraphicsView.ScrollHandDrag)
+        
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        
+        self.g_scene.addRect(self.PAN_BOUNDARY)        
+    
+    def wheelEvent(self, event):   
+#         print(event.angleDelta())  
+        
+        if (event.angleDelta().y() > 0):
+            self.scale(1.2, 1.2)
+        else:
+            self.scale(0.9, 0.9)
