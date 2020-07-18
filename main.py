beefafad7ce3168f16ba5a6551659e7c93f2ab69
Maximo van der Raadt <37664376+InlandTaipan@users.noreply.github.com>
2020-07-18 13:23:59 -0600

    Finished BridgeCost and force validity
    :)

--- main.py
+++ main.py
@@ -12,19 +12,31 @@ if __name__ == "__main__":
     print('Bridge Analyser active!')
     
     extractor = DataExtractor() 
-    output_list = extractor.extract_data("test_data_extract3.csv")    
+    #output_list = extractor.extract_data("test_data_extract3.csv")
+    output_list = extractor.extract_data("caveman_bridge_output.csv")  
 
     ss = anastruct.SystemElements()
     
     nodeDict, bridgeCost = coolCalculator.createMembers(output_list, ss)
 
-    print(nodeDict)
+    #print(nodeDict)
     
     coolCalculator.addSupports(ss, nodeDict)
     coolCalculator.addLoads(ss, nodeDict)
 
-    print("Cost: $" + str(bridgeCost))
+    ss.solve()
+    
+    forceDict = coolCalculator.returnForceDict(ss)
+     
+    if coolCalculator.isValidForces(forceDict):
+        print("Very valid, much wow")
+        bridgeCost = coolCalculator.updateCost(forceDict, bridgeCost)
+        print("Cost: $" + str(bridgeCost))
+    else:
+        print("Not valid, very sad :(")
+        print("Cost: Your bridge is bad and so are you")    
 
     coolCalculator.runSimulation(ss)
-    print(ss.get_element_results())
+    
+    print(forceDict)
  
